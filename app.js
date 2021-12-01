6bc136386dadbfaae7adcf5b6bb14855126a078c
billmills <you@example.com>
2021-12-01 18:52:39 -0500

    simplify promise spaghetti

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 const nodemailer = require('nodemailer');
+const crypto = require("crypto")
 const upload = multer();
 const app = express();
 
-var User = require('./service/userService');
+const user = require('./models/user');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -14,65 +15,59 @@ app.get('/', function(req, res, next) {
     res.render('index');
 });
 
-app.post('/register_key', upload.none(), function(req, res, next){User.createUser(req.body.first,req.body.last,req.body.affiliation,req.body.email).then(
-    function (response) {
-        // TBD: send success email to response.email
-        res.render('reg-success')
-    },
-    function (response) {
-        if(response.code == 11000){
-            // TBD: resend response.token to response.email
-            res.render('reg-dupe')
-            return
-        }
-    })
-    .catch(function (response) {
-        res.send('catchbucket');
+app.post('/register_key', upload.none(), function(req, res, next){
+
+    let k = crypto.randomBytes(20).toString('hex')
+    user.create({
+        first: req.body.first, 
+        last: req.body.last, 
+        email: req.body.email, 
+        affiliation: req.body.affiliation, 
+        key: k, 
+        tokenValid: 1}).then(function(response){
+            // TBD: send success email to response.email
+            res.render('reg-success')
+            return          
+        }).catch(function(response){
+            if(response.code == 11000){
+                // TBD: resend response.token to response.email
+                res.render('reg-dupe')
+                return
+            }
+        })
+});
+
+app.post('/recover_key', upload.none(), function(req, res, next){
+
+    user.findOne({"email": req.body['recover-email']}).exec().then(function(response){
+        // TBD: send response.key to response.email
+        console.log(response)
+        res.render('recover-success')        
+    }).catch(function(response){
+        return null       
     });
 });
 
-app.post('/recover_key', upload.none(), function(req, res, next){User.findUser(req.body['recover-email']).then(
-    function (response) {
-        // TBD: send response.key to response.email
-        res.render('recover-success')
-    },
-    function (response) {
-        if(response.code == 404){
-            res.render('recover-fail')
-        }
-    })
-    .catch(function (response) {
-        res.send('catchbucket');
-    });
-});
+app.post('/rotate_key', upload.none(), function(req, res, next){
 
-app.post('/rotate_key', upload.none(), function(req, res, next){User.rotateKey(req.body['rotate-email'], req.body['rotate-key']).then(
-    function (response) {
-        // TBD: send response.key to response.email
+    let k = crypto.randomBytes(20).toString('hex')
+    user.findOneAndUpdate({"email": req.body['rotate-email'], "key": req.body['rotate-key']}, {"key": k},{"new": true}).then(function(response){
+        // TBD: send response.key to response.email if an update was made
         res.render('rotate-success')
-    },
-    function (response) {
-        if(response.code == 404){
-            res.render('rotate-fail')
-        }
+    }).catch(function(response){
+        return null
     })
-    .catch(function (response) {
-        res.send('catchbucket');
-    });
 });
 
-app.post('/delete_key', upload.none(), function(req, res, next){User.deleteKey(req.body['delete-email'], req.body['delete-key']).then(
-    function (response) {
+app.post('/delete_key', upload.none(), function(req, res, next){
+
+    user.deleteOne({"email": req.body['delete-email'], "key": req.body['delete-key']}).then(function(response){
+        // TBD: email confirming deletion
+        console.log(response)
         res.render('delete-success')
-    },
-    function (response) {
-        if(response.code == 404){
-            res.render('delete-fail')
-        }
+    }).catch(function(response){
+        return null
     })
-    .catch(function (response) {
-        res.send('catchbucket');
-    });
 });
 
 module.exports = app;
